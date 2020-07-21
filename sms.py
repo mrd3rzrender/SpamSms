939623c3f40fb07931493204600996261d247942
mrd3rzrender <63225625+mrd3rzrender@users.noreply.github.com>
2020-07-22 01:03:11 +0700

    Create sms.py

---
+++ sms.py
@@ -0,0 +1,35 @@
+import os,sys,time
+import requests
+import subprocess
+from requests import post
+
+#SpamSms Derz Grub
+#Tools ini sekarang not free 
+#jika mau silahkan chat ke nomer ini 08812425716
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def bersih():
+    os.system("clear")
